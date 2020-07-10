8f2920d44f8007210018df52c9bc8eb45818111d
Graciela <graziellats501@gmail.com>
2020-07-09 21:35:54 -0500

    Función de bienvenida

--- app/src/main/java/com/example/repotutorial/MainActivity.java
+++ app/src/main/java/com/example/repotutorial/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.repotutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mensaje();
     }
 
-    private void suma(){
-
+    private void mensaje(){
+        Toast.makeText(this, "Sean todos bienvenidos!", Toast.LENGTH_SHORT).show();
     }
 }
